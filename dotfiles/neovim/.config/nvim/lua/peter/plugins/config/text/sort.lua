38ad838355e38aa465f67120b5cf68ffd0811d6b
Peter Tri Ho <mail@petertriho.com>
2024-06-29 11:40:34 +0000

    fix(nvim): sort keymap

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/sort.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/sort.lua
@@ -2,7 +2,7 @@ return {
     "sQVe/sort.nvim",
     cmd = "Sort",
     keys = {
-        { "<leader>s", ":Sort i<CR>", mode = "x", desc = "sort" },
+        { "<leader>as", ":Sort i<CR>", mode = "x", desc = "sort" },
     },
     config = true,
 }
