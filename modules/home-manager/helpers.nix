cb9f522dd1189b9e78192876b71b5f2ff8949a91
Peter Tri Ho <mail@petertriho.com>
2024-07-01 02:21:00 +0000

    refactor(nix): move to `.nix-config`

--- modules/home-manager/helpers.nix
+++ modules/home-manager/helpers.nix
@@ -8,7 +8,7 @@ with lib; {
   # https://github.com/nix-community/home-manager/issues/676
   config = {
     lib.meta = {
-      configPath = "${config.home.homeDirectory}/nix-config";
+      configPath = "${config.home.homeDirectory}/.nix-config";
       mkDotfilesSymlink = path:
         config.lib.file.mkOutOfStoreSymlink
         (config.lib.meta.configPath + "/dotfiles/" + path);
