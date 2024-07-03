f05499874ac0e070c25b62ecc879a0bc94967e79
Peter Tri Ho <mail@petertriho.com>
2024-07-03 03:15:14 +0000

    feat(nvim): add descriptions to `winshift` keymaps

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/winshift.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/winshift.lua
@@ -2,14 +2,14 @@ return {
     "sindrets/winshift.nvim",
     cmd = "WinShift",
     keys = {
-        { "<C-w><CR>", "<CMD>WinShift swap<CR>" },
-        { "<C-w>m", "<CMD>WinShift swap<CR>" },
-        { "<C-w><C-e>", "<CMD>WinShift<CR>" },
-        { "<C-w>e", "<CMD>WinShift<CR>" },
-        { "<C-M-j>", "<CMD>WinShift down<CR>" },
-        { "<C-M-k>", "<CMD>WinShift up<CR>" },
-        { "<C-M-h>", "<CMD>WinShift left<CR>" },
-        { "<C-M-l>", "<CMD>WinShift right<CR>" },
+        { "<C-w><CR>", "<CMD>WinShift swap<CR>", desc = "Swap" },
+        { "<C-w>m", "<CMD>WinShift swap<CR>", desc = "Swap" },
+        { "<C-w><C-e>", "<CMD>WinShift<CR>", desc = "WinShift" },
+        { "<C-w>e", "<CMD>WinShift<CR>", desc = "WinShift" },
+        { "<C-M-j>", "<CMD>WinShift down<CR>", desc = "Down" },
+        { "<C-M-k>", "<CMD>WinShift up<CR>", desc = "Up" },
+        { "<C-M-h>", "<CMD>WinShift left<CR>", desc = "Left" },
+        { "<C-M-l>", "<CMD>WinShift right<CR>", desc = "Right" },
     },
     config = function()
         require("winshift").setup({
