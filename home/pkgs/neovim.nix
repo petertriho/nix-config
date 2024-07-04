bbb2eba6a81282afd882f4c90143769b4bc4c01e
Peter Tri Ho <mail@petertriho.com>
2024-07-04 06:59:56 +0000

    style(nix): reorder `sort-package-json`

--- home/pkgs/neovim.nix
+++ home/pkgs/neovim.nix
@@ -30,19 +30,6 @@
       postInstall = removePythonLicense;
     });
 
-  sort-package-json = pkgs.buildNpmPackage rec {
-    pname = "sort-package-json";
-    version = "2.10.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "keithamus";
-      repo = pname;
-      rev = "v${version}";
-      hash = "sha256-JiOQI3oUH4TaCWd8rx8796vXNhwior380PlQfjQXMzA=";
-    };
-    npmDepsHash = "sha256-wKs7x1OGX89xT698i3WAz5iNsv71nbmYe8F9DjXO3tI=";
-    dontNpmBuild = true;
-  };
-
   pyemojify = with pkgs.python3Packages;
     buildPythonPackage rec {
       pname = "pyemojify";
@@ -85,6 +72,19 @@
         pygments
       ];
     };
+
+  sort-package-json = pkgs.buildNpmPackage rec {
+    pname = "sort-package-json";
+    version = "2.10.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "keithamus";
+      repo = pname;
+      rev = "v${version}";
+      hash = "sha256-JiOQI3oUH4TaCWd8rx8796vXNhwior380PlQfjQXMzA=";
+    };
+    npmDepsHash = "sha256-wKs7x1OGX89xT698i3WAz5iNsv71nbmYe8F9DjXO3tI=";
+    dontNpmBuild = true;
+  };
 
   ssort = with pkgs.python3Packages;
     buildPythonApplication rec {
