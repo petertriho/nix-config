3c6c35845e7251af6a523394e9fa542ab5b5e94a
Peter Tri Ho <mail@petertriho.com>
2024-07-05 08:58:57 +0000

    feat(nix): add support for windows docker desktop

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,6 +1,9 @@
 {
   inputs,
   outputs,
+  config,
+  lib,
+  pkgs,
   ...
 }: {
   imports = [
@@ -8,9 +11,32 @@
     inputs.nixos-wsl.nixosModules.wsl
   ];
 
-  wsl.enable = true;
-  wsl.defaultUser = "peter";
+  wsl = {
+    enable = true;
+    defaultUser = "peter";
+    wslConf.interop.appendWindowsPath = false;
+  };
   networking.hostName = "wsl";
+
+  # NOTE: Docker Desktop https://github.com/nix-community/NixOS-WSL/issues/235#issuecomment-1937424376
+  wsl = {
+    docker-desktop.enable = false;
+    extraBin = with pkgs; [
+      {src = "${coreutils}/bin/mkdir";}
+      {src = "${coreutils}/bin/cat";}
+      {src = "${coreutils}/bin/whoami";}
+      {src = "${coreutils}/bin/ls";}
+      {src = "${busybox}/bin/addgroup";}
+      {src = "${su}/bin/groupadd";}
+      {src = "${su}/bin/usermod";}
+    ];
+  };
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+  systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 
   system.stateVersion = "23.11";
 }
