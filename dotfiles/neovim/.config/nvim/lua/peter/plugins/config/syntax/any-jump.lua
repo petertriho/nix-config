82c992068be3e50c2cc892178789ae3553b0e039
Peter Tri Ho <mail@petertriho.com>
2024-07-07 06:06:26 +0000

    fix(nvim): add descriptions for `any-jump`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/syntax/any-jump.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/syntax/any-jump.lua
@@ -7,10 +7,10 @@ return {
         "AnyJumpLastResults",
     },
     keys = {
-        { "gj", "<CMD>AnyJump<CR>" },
-        { "gJ", "<CMD>AnyJumpBack<CR>" },
-        { "gk", "<CMD>AnyJumpLastResults<CR>" },
-        { "gj", "<CMD>AnyJumpVisual<CR>", mode = "x" },
+        { "gj", "<CMD>AnyJump<CR>", desc = "AnyJump" },
+        { "gJ", "<CMD>AnyJumpBack<CR>", desc = "AnyJumpBack" },
+        { "gk", "<CMD>AnyJumpLastResults<CR>", desc = "AJLastResults" },
+        { "gj", "<CMD>AnyJumpVisual<CR>", mode = "x", desc = "AnyJumpVisual" },
     },
     init = function()
         vim.g.any_jump_disable_default_keybindings = 1
