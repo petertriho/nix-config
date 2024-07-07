302e54817f3a6f153820aa7212d39af34977d85a
Peter Tri Ho <mail@petertriho.com>
2024-07-07 06:38:41 +0000

    refactor(nvim): remove duplicate (with default/upstream) lsp keymaps

--- dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
@@ -11,12 +11,6 @@ local function on_attach(client, bufnr)
         vim.keymap.set(mode, lhs, rhs, opts)
     end
 
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
     if client.name == "basedpyright" then
         buf_set_keymap("n", "<leader>o", "<CMD>PyrightOrganizeImports<CR>", { desc = "organize-imports" })
     elseif client.name == "ruff" then
@@ -31,8 +25,6 @@ local function on_attach(client, bufnr)
         "<CMD>lua vim.diagnostic.open_float(0, { scope = 'line', source = 'always', border = 'rounded' })<CR>",
         { desc = "diagnostic" }
     )
-    buf_set_keymap("n", "[d", "<CMD>lua vim.diagnostic.goto_prev()<CR>", { desc = "diagnostic-next" })
-    buf_set_keymap("n", "]d", "<CMD>lua vim.diagnostic.goto_next()<CR>", { desc = "diagnostic-prev" })
     buf_set_keymap("n", "<leader>tq", "<CMD>lua vim.diagnostic.setqflist()<CR>", { desc = "qflist-diagnostics" })
 
     if client.server_capabilities.codeActionProvider then
@@ -45,41 +37,22 @@ local function on_attach(client, bufnr)
             buffer = bufnr,
             desc = "LSP code action lightbulb",
         })
-
-        buf_set_keymap("n", "<leader>k", "<CMD>lua vim.lsp.buf.code_action()<CR>", { desc = "code-actions" })
-        buf_set_keymap("v", "<leader>k", "<CMD>lua vim.lsp.buf.range_code_action()<CR>", { desc = "code-actions" })
-    end
-
-    if client.server_capabilities.definitionProvider then
-        buf_set_keymap("n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
     end
 
     if client.server_capabilities.declarationProvider then
-        buf_set_keymap("n", "gl", "<CMD>lua vim.lsp.buf.declaration()<CR>", { desc = "Go to declaration" })
+        buf_set_keymap("n", "grd", "<CMD>lua vim.lsp.buf.declaration()<CR>", { desc = "Declaration" })
     end
 
     if client.server_capabilities.documentSymbolProvider then
         require("nvim-navic").attach(client, bufnr)
     end
 
-    if client.server_capabilities.hoverProvider then
-        buf_set_keymap("n", "K", "<CMD>lua vim.lsp.buf.hover()<CR>", { desc = "hover" })
-    end
-
     if client.server_capabilities.implementationProvider then
-        buf_set_keymap("n", "gm", "<CMD>lua vim.lsp.buf.implementation()<CR>", { desc = "Go to implementation" })
-    end
-
-    if client.server_capabilities.renameProvider then
-        buf_set_keymap("n", "<leader>ar", "<CMD>lua vim.lsp.buf.rename()<CR>", { desc = "rename" })
-    end
-
-    if client.server_capabilities.referencesProvider then
-        buf_set_keymap("n", "grr", "<CMD>lua vim.lsp.buf.references()<CR>", { desc = "references" })
+        buf_set_keymap("n", "gri", "<CMD>lua vim.lsp.buf.implementation()<CR>", { desc = "Go to implementation" })
     end
 
     if client.server_capabilities.signatureHelpProvider then
-        buf_set_keymap("n", "gs", "<CMD>lua vim.lsp.buf.signature_help()<CR>", { desc = "signature" })
+        buf_set_keymap("n", "grs", "<CMD>lua vim.lsp.buf.signature_help()<CR>", { desc = "Signature help" })
     end
 
     if client.server_capabilities.typeDefinitionProvider then
