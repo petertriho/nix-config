269f5b134d5596f582df3b08c708eba6302e48e5
Peter Tri Ho <mail@petertriho.com>
2024-07-13 09:18:23 +0000

    fix(nvim): `vim-abolish` and `vim-caser` not loading

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-abolish.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-abolish.lua
@@ -1,5 +1,17 @@
 return {
     "petertriho/vim-abolish",
     cmd = { "Subvert", "S" },
-    keys = { { "cr", mode = "n" } },
+    keys = {
+        "cr ",
+        "cr-",
+        "cr.",
+        "cr_",
+        "crc",
+        "crk",
+        "crm",
+        "crs",
+        "crt",
+        "cru",
+        "crU",
+    },
 }
--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-caser.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-caser.lua
@@ -1,6 +1,20 @@
 return {
     "arthurxavierx/vim-caser",
-    keys = { { "cC", mode = "n" } },
+    keys = {
+        "cC ",
+        "cC-",
+        "cC.",
+        "cC_",
+        "cCc",
+        "cCk",
+        "cCK",
+        "cCm",
+        "cCp",
+        "cCs",
+        "cCt",
+        "cCu",
+        "cCU",
+    },
     init = function()
         vim.g.caser_prefix = "cC"
     end,
