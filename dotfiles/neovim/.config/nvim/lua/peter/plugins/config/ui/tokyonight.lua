c2aca988b4b8c1e54860ddc031a6b434497fba15
Peter Tri Ho <mail@petertriho.com>
2024-07-13 12:46:46 +0000

    style(nvim): disable `linehl` for diagnostics

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ui/tokyonight.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ui/tokyonight.lua
@@ -65,21 +65,8 @@ return {
                 text = {
                     [vim.diagnostic.severity.ERROR] = "󰅚 ",
                     [vim.diagnostic.severity.WARN] = "󰀪 ",
-                    [vim.diagnostic.severity.INFO] = " ",
                     [vim.diagnostic.severity.HINT] = "󰌶 ",
-                },
-                linehl = {
-                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-                    [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-                    [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-                    [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-                },
-                numhl = {
-
-                    [vim.diagnostic.severity.ERROR] = "",
-                    [vim.diagnostic.severity.WARN] = "",
-                    [vim.diagnostic.severity.INFO] = "",
-                    [vim.diagnostic.severity.HINT] = "",
+                    [vim.diagnostic.severity.INFO] = " ",
                 },
             },
         })
