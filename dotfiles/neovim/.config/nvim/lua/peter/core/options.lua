4c7f7b37eaa7aaf79c9bfcb259b2bfd4eedd39b0
Peter Tri Ho <mail@petertriho.com>
2024-07-15 06:06:26 +0000

    fix(nvim): `win32yank` not being used

--- dotfiles/neovim/.config/nvim/lua/peter/core/options.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/core/options.lua
@@ -84,7 +84,7 @@ opt.wildignore = {
 }
 
 -- Clipboard
-if vim.fn.exists("$TMUX") == 0 and vim.fn.has("wsl") == 1 and vim.fn.executable("win32yank") == 0 then
+if vim.fn.exists("$TMUX") == 0 and vim.fn.has("wsl") == 1 and vim.fn.executable("win32yank.exe") == 0 then
     -- Use native clip.exe if win32yank does not exist
     g.clipboard = {
         name = "WslClipboard",
