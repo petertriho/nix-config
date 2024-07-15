42cf9d801873c01b1e89fd2f86d0fe3593fa2f4e
Peter Tri Ho <mail@petertriho.com>
2024-07-15 06:51:25 +0000

    fix(nvim): `nvim-ts-autotag` not working

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/treesitter/nvim-ts-autotag.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/treesitter/nvim-ts-autotag.lua
@@ -1,7 +1,5 @@
 return {
     "windwp/nvim-ts-autotag",
     event = "User LoadedNvimTreesitter",
-    config = function()
-        require("nvim-treesitter.configs").setup({ autotag = { enable = true } })
-    end,
+    config = true,
 }
