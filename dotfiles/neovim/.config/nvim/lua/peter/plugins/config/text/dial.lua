31f3d3f8ae68df63fb37a2297a2c8821bb2dc941
Peter Tri Ho <mail@petertriho.com>
2024-07-16 05:17:30 +0000

    fix(nvim): g-increment/decrement not working

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/dial.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/dial.lua
@@ -5,7 +5,7 @@ return {
         "<Plug>(dial-decrement)",
         { "<C-a>", "<Plug>(dial-increment)", mode = { "n", "v" } },
         { "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "v" } },
-        { "g<C-a>", "g<Plug>(dial-increment)", mode = "v" },
-        { "g<C-x>", "g<Plug>(dial-decrement)", mode = "v" },
+        { "g<C-a>", "g<Plug>(dial-increment)", mode = "v", remap = true },
+        { "g<C-x>", "g<Plug>(dial-decrement)", mode = "v", remap = true },
     },
 }
