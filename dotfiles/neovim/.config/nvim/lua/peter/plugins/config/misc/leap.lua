c8449d7614ecd3d6c7d1df4b3e7369dfa4d0e71d
Peter Tri Ho <mail@petertriho.com>
2024-07-16 04:40:42 +0000

    feat(nvim): add `flash.nvim`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/leap.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/leap.lua
@@ -1,17 +1,20 @@
 return {
     {
         "ggandor/leap.nvim",
+        enabled = false,
         config = function()
             require("leap").create_default_mappings()
         end,
     },
     {
         "ggandor/leap-spooky.nvim",
+        enabled = false,
         dependencies = { "ggandor/leap.nvim" },
         config = true,
     },
     {
         "ggandor/flit.nvim",
+        enabled = false,
         dependencies = { "ggandor/leap.nvim" },
         opts = {
             labeled_modes = "nvo",
