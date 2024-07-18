3b9eff04d4c15d6351f3dbcbc6f20f62b1a7d350
Peter Tri Ho <mail@petertriho.com>
2024-07-18 04:24:59 +0000

    feat(nvim): simplify lazy loading for `vim-abolish` and `vim-caser`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-abolish.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-abolish.lua
@@ -2,20 +2,9 @@ return {
     "petertriho/vim-abolish",
     cmd = { "Subvert", "S" },
     keys = {
-        "<leader>c ",
-        "<leader>c-",
-        "<leader>c.",
-        "<leader>c_",
-        "<leader>cc",
-        "<leader>ck",
-        "<leader>cm",
-        "<leader>cs",
-        "<leader>ct",
-        "<leader>cu",
-        "<leader>cU",
+        { "<leader>c", "<Plug>(abolish-coerce-word)", desc = "coerce" },
     },
     init = function()
         vim.g.abolish_no_mappings = 1
-        vim.keymap.set("n", "<leader>c", "<Plug>(abolish-coerce-word)", { desc = "coerce" })
     end,
 }
--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-caser.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-caser.lua
@@ -1,21 +1,9 @@
 return {
     "arthurxavierx/vim-caser",
     keys = {
-        "<leader>C ",
-        "<leader>C-",
-        "<leader>C.",
-        "<leader>C_",
-        "<leader>Cc",
-        "<leader>Ck",
-        "<leader>CK",
-        "<leader>Cm",
-        "<leader>Cp",
-        "<leader>Cs",
-        "<leader>Ct",
-        "<leader>Cu",
-        "<leader>CU",
+        "<leader>C",
     },
     init = function()
-        vim.g.caser_prefix = "<leader>C"
+        vim.g.caserPrefix = "<leader>C"
     end,
 }
