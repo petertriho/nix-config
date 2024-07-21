336447bee89867fa84281fd967ecbf2841dddf12
Peter Tri Ho <mail@petertriho.com>
2024-07-21 08:28:04 +0000

    style(nvim): simplify `stickybuf` specs

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/stickybuf.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/stickybuf.lua
@@ -1,18 +1,16 @@
 return {
     "stevearc/stickybuf.nvim",
-    config = function()
-        require("stickybuf").setup({
-            get_auto_pin = function(bufnr)
-                local filetype = vim.bo[bufnr].filetype
+    opts = {
+        get_auto_pin = function(bufnr)
+            local filetype = vim.bo[bufnr].filetype
 
-                for _, v in ipairs(require("peter.plugins.filetypes").sidebars) do
-                    if v == filetype then
-                        return "filetype"
-                    end
+            for _, v in ipairs(require("peter.plugins.filetypes").sidebars) do
+                if v == filetype then
+                    return "filetype"
                 end
+            end
 
-                return require("stickybuf").should_auto_pin(bufnr)
-            end,
-        })
-    end,
+            return require("stickybuf").should_auto_pin(bufnr)
+        end,
+    },
 }
