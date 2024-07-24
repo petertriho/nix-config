acec4ca22f738ee1e2fe268404a84e436ab00aa2
Peter Tri Ho <mail@petertriho.com>
2024-07-24 09:27:22 +0000

    feat(nvim): add `nvim-highlight-colors`
    replaces `nvim-colorizer`

---
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/syntax/nvim-highlight-colors.lua
@@ -0,0 +1,11 @@
+return {
+    "brenoprata10/nvim-highlight-colors",
+    event = "User LazyLoadFile",
+    opts = {
+        render = "virtual",
+        virtual_symbol_prefix = " ",
+        virtual_symbol_suffix = "",
+        virtual_symbol_position = "eow",
+        enable_tailwind = true,
+    },
+}
