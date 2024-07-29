b9a15c7a6d86c91ec5f88bd55abb444b811733ce
Peter Tri Ho <mail@petertriho.com>
2024-07-29 14:14:39 +0000

    refactor(nvim): move delete all buffers to `<leader>D`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/close-buffers.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/close-buffers.lua
@@ -4,7 +4,7 @@ return {
     keys = {
         { "<leader>.", "<CMD>BWipeout other<CR>", desc = "only-buffer" },
         { "<leader>,", "<CMD>BWipeout hidden<CR>", desc = "hidden-buffers" },
-        { "<leader>x", "<CMD>BWipeout all<CR>", desc = "all-buffers" },
+        { "<leader>D", "<CMD>BWipeout all<CR>", desc = "all-buffers" },
     },
     config = true,
 }
