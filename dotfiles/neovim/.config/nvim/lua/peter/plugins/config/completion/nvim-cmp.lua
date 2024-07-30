c721e82d296811f14cbaf006c78d26e5c5b767ec
Peter Tri Ho <mail@petertriho.com>
2024-07-30 15:29:44 +0000

    perf(nvim): disable `cmp-buffer` on big files

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/nvim-cmp.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/nvim-cmp.lua
@@ -221,7 +221,10 @@ return {
                             -- visible buffers
                             local bufs = {}
                             for _, win in ipairs(vim.api.nvim_list_wins()) do
-                                bufs[vim.api.nvim_win_get_buf(win)] = true
+                                buf = vim.api.nvim_win_get_buf(win)
+                                if not require("peter.core.utils").file_is_big(buf) then
+                                    bufs[buf] = true
+                                end
                             end
                             return vim.tbl_keys(bufs)
                         end,
