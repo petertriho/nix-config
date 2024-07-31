7fa7b5f16c80ff0e2c6139091a83c992d2e48fbb
Peter Tri Ho <mail@petertriho.com>
2024-07-31 11:23:13 +0000

    refactor(fish): move delta_side_by_side into `config.fish`

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -12,8 +12,6 @@ if type --query fzf_configure_bindings
         --variables=\e\ce
 end
 
-delta_side_by_side
-
 set -g sponge_delay 10
 set -g sponge_purge_only_on_exit true
 
@@ -21,7 +19,22 @@ if type --query zoxide
     zoxide init fish | source
 end
 
+function delta_side_by_side --on-signal WINCH
+    set -x COLUMNS $COLUMNS
+
+    if test "$COLUMNS" -ge 180; and ! contains side-by-side "$DELTA_FEATURES"
+        set --global --export --append DELTA_FEATURES side-by-side
+    else if test "$COLUMNS" -lt 180; and contains side-by-side "$DELTA_FEATURES"
+        set --erase DELTA_FEATURES[(contains --index side-by-side "$DELTA_FEATURES")]
+    end
+end
+
+set -g ls_cmd ls
+
+if type --query eza
+    set -g ls_cmd eza
+end
+
 function ls_on_cd --on-variable PWD
-    # ls
-    eza --classify
+    $ls_cmd --classify
 end
