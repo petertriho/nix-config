1432407f8be85485826b9ac9bcf5d59875d9abc5
Peter Tri Ho <mail@petertriho.com>
2024-07-31 06:18:20 +0000

    refactor(nvim): change `ibl` hook to `setup_buffer`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/syntax/indent-blankline.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/syntax/indent-blankline.lua
@@ -10,10 +10,5 @@ return {
                 show_start = false,
             },
         })
-
-        local hooks = require("ibl.hooks")
-        hooks.register(hooks.type.ACTIVE, function(bufnr)
-            return not require("peter.core.utils").file_is_big(bufnr)
-        end)
     end,
 }
