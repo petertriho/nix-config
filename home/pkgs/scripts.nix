eea69a21f3462d2e1aa903eea7fd0ac7665205ea
Peter Tri Ho <mail@petertriho.com>
2024-08-06 05:34:25 +0000

    style(scripts): format generated `flake.nix` with `nixfmt`

--- home/pkgs/scripts.nix
+++ home/pkgs/scripts.nix
@@ -4,4 +4,5 @@
 
   home.file.".local/bin".source = config.lib.meta.mkDotfilesSymlink "scripts/.local/bin";
   xdg.configFile."fish/completions/update.fish".source = config.lib.meta.mkDotfilesSymlink "scripts/.config/fish/completions/update.fish";
+  xdg.configFile."fish/completions/nix-direnv-helper.fish".source = config.lib.meta.mkDotfilesSymlink "scripts/.config/fish/completions/nix-direnv-helper.fish";
 }
