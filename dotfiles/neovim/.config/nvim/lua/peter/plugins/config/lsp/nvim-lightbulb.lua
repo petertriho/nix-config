76efacba7bf6be879a825914374c803be57961de
Peter Tri Ho <mail@petertriho.com>
2024-08-08 06:09:07 +0000

    fix: use fork of `nvim-lightbulb`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/nvim-lightbulb.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/nvim-lightbulb.lua
@@ -1,7 +1,7 @@
 return {
     -- NOTE: vim.lsp.get_active_clients is deprecated, using fork until PR is merged
     -- "kosayoda/nvim-lightbulb"
-    "gh-liu/nvim-lightbulb",
+    "petertriho/nvim-lightbulb",
     event = "User LazyLoadFile",
     opts = {
         link_highlights = false,
