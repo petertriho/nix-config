25296ee1aa4383fd32e7cbca63c57aae60b33942
Peter Tri Ho <mail@petertriho.com>
2024-08-29 20:41:46 +1000

    feat(darwin): set `system.defaults`

--- systems/darwin/base.nix
+++ systems/darwin/base.nix
@@ -69,6 +69,43 @@
     ];
   };
 
-  system.configurationRevision = outputs.rev or outputs.dirtyRev or null;
-  system.stateVersion = lib.mkDefault 4;
+  system = {
+    defaults = {
+      LaunchServices = {
+        LSQuarantine = false;
+      };
+      NSGlobalDomain = {
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.sound.beep.volume" = 0.0;
+        ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSWindowShouldDragOnGesture = true;
+      };
+      dock = {
+        autohide = true;
+        mineffect = "scale";
+        mru-spaces = false;
+        show-recents = false;
+        tilesize = 48;
+        # persistent-apps = [];
+        # persistent-others = [];
+      };
+      finder = {
+        _FXShowPosixPathInTitle = false;
+        QuitMenuItem = true;
+      };
+    };
+    configurationRevision = outputs.rev or outputs.dirtyRev or null;
+    stateVersion = lib.mkDefault 4;
+  };
 }
