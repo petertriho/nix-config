7944f9b973b4d4de44bdac880fd918e5fda110d3
Peter Tri Ho <mail@petertriho.com>
2024-08-30 17:32:11 +1000

    fix(darwin): `vim` system clipboard not working

--- systems/base.nix
+++ systems/base.nix
@@ -28,28 +28,47 @@
 
   environment = {
     systemPackages = with pkgs; [
-      ((vim_configurable.override { }).customize {
-        vimrcConfig.customRC =
-          # vim
-          ''
-            set hlsearch
-            set ignorecase
-            set incsearch
-            set number
-            set noswapfile
+      (
+        (vim-full.override (
+          {
+            luaSupport = false;
+            perlSupport = false;
+            pythonSupport = false;
+            rubySupport = false;
+          }
+          // (
+            if pkgs.stdenv.isDarwin then
+              {
+                darwinSupport = true;
+                guiSupport = false;
+              }
+            else
+              { }
+          )
+        )).customize
+        {
+          vimrcConfig.customRC =
+            # vim
+            ''
+              set hlsearch
+              set ignorecase
+              set incsearch
+              set number
+              set noswapfile
 
-            set autoread
-            set backspace=indent,eol,start
-            set laststatus=2
-            set lazyredraw
-            set relativenumber
-            set ruler
-            set wildmenu
+              set autoread
+              set backspace=indent,eol,start
+              set laststatus=2
+              set lazyredraw
+              set relativenumber
+              set ruler
+              set wildmenu
 
-            filetype plugin indent on
-            syntax on
-          '';
-      })
+              filetype plugin indent on
+              syntax on
+            '';
+        }
+      )
     ];
     variables.EDITOR = "vim";
   };
