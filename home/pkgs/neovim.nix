ab290a45d5b728052d0bda523fb4510afc3880a2
Peter Tri Ho <mail@petertriho.com>
2024-09-23 05:42:41 +0000

    refactor(nix): switch to `fetchgit` for deps originally `git` in `superhtml`

--- home/pkgs/neovim.nix
+++ home/pkgs/neovim.nix
@@ -157,12 +157,14 @@ let
     {
       linkFarm,
       fetchzip,
+      fetchgit,
     }:
     linkFarm "zig-packages" [
       {
         name = "1220b9ce6dc0e1fbcd9e7b406ab164344f81774351495f860a90729187c3c058ef4f";
-        path = fetchzip {
-          url = "https://github.com/kristoff-it/zig-lsp-kit/archive/b4bf61d7fbf9cf7cfdb6f01b211947d2de3e42fd.tar.gz";
+        path = fetchgit {
+          url = "https://github.com/kristoff-it/zig-lsp-kit";
+          rev = "b4bf61d7fbf9cf7cfdb6f01b211947d2de3e42fd";
           hash = "sha256-6mlnPTLBXZQwWXstV+h1PAKtMq8RGcJM2dRJ8NqqqtU=";
         };
       }
@@ -175,29 +177,33 @@ let
       }
       {
         name = "12209cde192558f8b3dc098ac2330fc2a14fdd211c5433afd33085af75caa9183147";
-        path = fetchzip {
-          url = "https://github.com/ziglibs/known-folders/archive/0ad514dcfb7525e32ae349b9acc0a53976f3a9fa.tar.gz";
+        path = fetchgit {
+          url = "https://github.com/ziglibs/known-folders.git";
+          rev = "0ad514dcfb7525e32ae349b9acc0a53976f3a9fa";
           hash = "sha256-X+XkFj56MkYxxN9LUisjnkfCxUfnbkzBWHy9pwg5M+g=";
         };
       }
       {
         name = "122014e78d7c69d93595993b3231f3141368e22634b332b0b91a2fb73a8570f147a5";
-        path = fetchzip {
-          url = "https://github.com/kristoff-it/scripty/archive/df8c11380f9e9bec34809f2242fb116d27cf39d6.tar.gz";
+        path = fetchgit {
+          url = "https://github.com/kristoff-it/scripty";
+          rev = "df8c11380f9e9bec34809f2242fb116d27cf39d6";
           hash = "sha256-qVm8pIfT1mHL1zanqYdFm/6AVH8poXKqLtz4+2j+F/A=";
         };
       }
       {
         name = "1220f2d8402bb7bbc4786b9c0aad73910929ea209cbd3b063842371d68abfed33c1e";
-        path = fetchzip {
-          url = "https://github.com/kristoff-it/zig-afl-kit/archive/f003bfe714f2964c90939fdc940d5993190a66ec.tar.gz";
+        path = fetchgit {
+          url = "https://github.com/kristoff-it/zig-afl-kit";
+          rev = "f003bfe714f2964c90939fdc940d5993190a66ec";
           hash = "sha256-tJ6Ln1SY4WjFZXUWQmgggsUfkd59QgmIpgdInMuv4PI=";
         };
       }
       {
         name = "1220010a1edd8631b2644476517024992f8e57f453bdb68668720bb590d168faf7c8";
-        path = fetchzip {
-          url = "https://github.com/allyourcodebase/AFLplusplus/archive/032984eabf5a35af386a3d0e542df7686da339c1.tar.gz";
+        path = fetchgit {
+          url = "https://github.com/allyourcodebase/AFLplusplus";
+          rev = "032984eabf5a35af386a3d0e542df7686da339c1";
           hash = "sha256-KB3QnKAQQ+5CKvJVrhMveMGpF3NTrlwpIyLHVIB96hs=";
         };
       }
