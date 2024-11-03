129d3afc365e21387ef299ebd658ebee83d25650
Peter Tri Ho <mail@petertriho.com>
2024-11-04 10:18:19 +1100

    chore(nix): update `flake.lock`

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
+      # TODO: revert back to master branch when merged
+      url = "github:nix-community/neovim-nightly-overlay/flake-update";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
