cbf3cd56ea189b14578013e038a1f00e38a89176
Peter Tri Ho <mail@petertriho.com>
2024-11-04 10:18:38 +1100

    fix(nix): disable `uasm` for yazi due to build errors

--- home/pkgs/yazi.nix
+++ home/pkgs/yazi.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    yazi
     # dependencies
     ffmpegthumbnailer
     file
     poppler
     unar
   ];
+
+  programs.yazi = {
+    enable = true;
+    # TODO: revert when https://github.com/NixOS/nixpkgs/issues/353119 is available
+    package = pkgs.yazi.override {
+      _7zz = pkgs._7zz.override { useUasm = true; };
+    };
+  };
 }
