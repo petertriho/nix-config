8b7bd3d241a2700393817cd6c5de1958143c6e41
Peter Tri Ho <mail@petertriho.com>
2024-11-13 20:05:53 +1100

    feat(nix): switch to `nixpkgs` `fish-lsp`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,6 @@
 with pkgs;
 rec {
   angular-language-server = callPackage ./angular-language-server { };
-  fish-lsp = callPackage ./fish-lsp { };
   pybetter = callPackage ./pybetter { inherit pkgs pyemojify; };
   pyemojify = callPackage ./pyemojify { };
   sort-package-json = callPackage ./sort-package-json { };
