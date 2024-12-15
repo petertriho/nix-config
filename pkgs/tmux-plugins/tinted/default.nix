1933da3272cd5dccf6432a79d8ad430a96fc5aa6
Peter Tri Ho <mail@petertriho.com>
2024-12-15 21:28:11 +1100

    chore(nix): update `tinted-tmux`

--- pkgs/tmux-plugins/tinted/default.nix
+++ pkgs/tmux-plugins/tinted/default.nix
@@ -5,11 +5,11 @@
 }:
 tmuxPlugins.mkTmuxPlugin {
   pluginName = "tmuxcolors";
-  version = "unstable-2024-10-21";
+  version = "unstable-2024-12-15";
   src = fetchFromGitHub {
     owner = "tinted-theming";
     repo = "tinted-tmux";
-    rev = "f0e7f7974a6441033eb0a172a0342e96722b4f14";
-    sha256 = "17mpv0s60kakhh1iwnsn67c6azfcwdlwar3zjxkykpdnq0a4926n";
+    rev = "20396f130e477512632c37f72590b71f59dbef88";
+    sha256 = "1hl86fb7c8vg87y33ffs39x8070q2jzpxa09ray2lqcp6bvxlbyp";
   };
 }
