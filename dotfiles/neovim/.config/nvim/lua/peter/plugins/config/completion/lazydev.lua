af2236799ba532bc0eac54e113a1039a41e0b409
Peter Tri Ho <mail@petertriho.com>
2025-01-01 14:59:02 +1100

    fix(nvim): `lazydev` conf

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/lazydev.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/lazydev.lua
@@ -4,7 +4,7 @@ return {
     opts = {
         library = {
             -- Load luvit types when the `vim.uv` word is found
-            { path = "luvit-meta/library", words = { "vim%.uv" } },
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
     },
 }
