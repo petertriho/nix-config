1c8f50644396ed3b1b61d2b57f8587b2e8f994e9
Peter Tri Ho <mail@petertriho.com>
2025-01-06 11:17:42 +1100

    revert(darwin): use `karabiner-elements` for `kanata`

--- systems/darwin/modules/kanata/default.nix
+++ systems/darwin/modules/kanata/default.nix
@@ -9,31 +9,34 @@ let
   KDK_DAEMON = "/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon";
 in
 {
-  system.activationScripts.postActivation.text =
-    #sh
-    ''
-      if [ ! -f ${KDK_MANAGER} ]; then
-        sudo /usr/sbin/installer -pkg ${KDK_PKG} -target /
-      fi
-
-      if [ -f ${KDK_MANAGER} ]; then
-        ${KDK_MANAGER} activate
-      else
-        "Karabiner DriverKit installation failed"
-      fi
-    '';
+  # system.activationScripts.postActivation.text =
+  #   #sh
+  #   ''
+  #     if [ ! -f ${KDK_MANAGER} ]; then
+  #       sudo /usr/sbin/installer -pkg ${KDK_PKG} -target /
+  #     fi
+  #
+  #     if [ -f ${KDK_MANAGER} ]; then
+  #       ${KDK_MANAGER} activate
+  #     else
+  #       "Karabiner DriverKit installation failed"
+  #     fi
+  #   '';
+  homebrew.casks = [
+    "karabiner-elements"
+  ];
 
   launchd.daemons = {
-    karabinerDriverKit = {
-      command = "sudo '${KDK_DAEMON}'";
-      serviceConfig = {
-        Label = "org.pqrs.karabiner.driverkit";
-        RunAtLoad = true;
-        KeepAlive = true;
-        StandardOutPath = "/Library/Logs/Karabiner-DriverKit-VirtualHIDDevice/out.log";
-        StandardErrorPath = "/Library/Logs/Karabiner-DriverKit-VirtualHIDDevice/error.log";
-      };
-    };
+    # karabinerDriverKit = {
+    #   command = "sudo '${KDK_DAEMON}'";
+    #   serviceConfig = {
+    #     Label = "org.pqrs.karabiner.driverkit";
+    #     RunAtLoad = true;
+    #     KeepAlive = true;
+    #     StandardOutPath = "/Library/Logs/Karabiner-DriverKit-VirtualHIDDevice/out.log";
+    #     StandardErrorPath = "/Library/Logs/Karabiner-DriverKit-VirtualHIDDevice/error.log";
+    #   };
+    # };
     kanata = {
       command = "sudo ${pkgs.kanata-macos}/bin/kanata -c /etc/kanata.kbd";
       serviceConfig = {
@@ -52,16 +55,16 @@ in
     ];
     etc = {
       "kanata.kbd".source = ./kanata.kbd;
-      "sudoers.d/karabiner-driverkit".source =
-        pkgs.runCommand "sudoers-karabiner-driverkit" { }
-          # sh
-          ''
-            BIN="${KDK_DAEMON}"
-            SHASUM=$(sha256sum "$BIN" | cut -d' ' -f1)
-            cat << EOF > "$out"
-            %admin ALL=(root) NOPASSWD: sha256:$SHASUM $BIN
-            EOF
-          '';
+      # "sudoers.d/karabiner-driverkit".source =
+      #   pkgs.runCommand "sudoers-karabiner-driverkit" { }
+      #     # sh
+      #     ''
+      #       BIN="${KDK_DAEMON}"
+      #       SHASUM=$(sha256sum "$BIN" | cut -d' ' -f1)
+      #       cat << EOF > "$out"
+      #       %admin ALL=(root) NOPASSWD: sha256:$SHASUM $BIN
+      #       EOF
+      #     '';
       "sudoers.d/kanata".source =
         pkgs.runCommand "sudoers-kanata" { }
           # sh
