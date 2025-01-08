d79248a35603409c8b370fc3947e1c496ab2df51
Peter Tri Ho <mail@petertriho.com>
2025-01-08 20:21:19 +1100

    chore(nvim): update `lazy-lock.json`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/blink-cmp.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/blink-cmp.lua
@@ -104,21 +104,10 @@ return {
             },
         },
         snippets = {
-            expand = function(snippet)
-                require("luasnip").lsp_expand(snippet)
-            end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require("luasnip").jumpable(filter.direction)
-                end
-                return require("luasnip").in_snippet()
-            end,
-            jump = function(direction)
-                require("luasnip").jump(direction)
-            end,
+            preset = "luasnip",
         },
         sources = {
-            default = { "lazydev", "lsp", "path", "luasnip", "buffer" },
+            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
             providers = {
                 lazydev = {
                     name = "LazyDev",
