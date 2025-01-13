37d942113f58f1a0f636d2cad83a57b2f305e36e
Peter Tri Ho <mail@petertriho.com>
2025-01-13 11:39:49 +1100

    feat(nvin): add `LiveEasyAlign` keybind

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-easy-align.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/text/vim-easy-align.lua
@@ -1,6 +1,14 @@
 return {
     "junegunn/vim-easy-align",
+    cmd = {
+        "EasyAlign",
+        "LiveEasyAlign",
+    },
     keys = {
         { "<leader>oa", "<Plug>(EasyAlign)", mode = { "n", "x" }, desc = "Align" },
+        { "<leader>ol", "<Plug>(LiveEasyAlign)", mode = { "n", "x" }, desc = "Live Align" },
     },
+    init = function()
+        vim.g.easy_align_ignore_groups = {}
+    end,
 }
