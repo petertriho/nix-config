5d28f5c8f2e1c2b6140b5d67bb22923e2323eb04
Peter Tri Ho <mail@petertriho.com>
2025-01-19 15:02:44 +1100

    revert(nvim): remove number keymaps from `snacks.pickers`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/snacks.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/snacks.lua
@@ -52,11 +52,6 @@ return {
                 enabled = true,
                 ui_select = true,
                 layouts = {
-                    -- select = {
-                    --     layout = {
-                    --         relative = "cursor",
-                    --     },
-                    -- },
                     default = {
                         layout = {
                             box = "horizontal",
@@ -86,16 +81,16 @@ return {
                 win = {
                     input = {
                         keys = {
-                            ["1"] = { "select_1", mode = { "n", "i" } },
-                            ["2"] = { "select_2", mode = { "n", "i" } },
-                            ["3"] = { "select_3", mode = { "n", "i" } },
-                            ["4"] = { "select_4", mode = { "n", "i" } },
-                            ["5"] = { "select_5", mode = { "n", "i" } },
-                            ["6"] = { "select_6", mode = { "n", "i" } },
-                            ["7"] = { "select_7", mode = { "n", "i" } },
-                            ["8"] = { "select_8", mode = { "n", "i" } },
-                            ["9"] = { "select_9", mode = { "n", "i" } },
-                            ["0"] = { "select_0", mode = { "n", "i" } },
+                            -- ["1"] = { "select_1", mode = { "n", "i" } },
+                            -- ["2"] = { "select_2", mode = { "n", "i" } },
+                            -- ["3"] = { "select_3", mode = { "n", "i" } },
+                            -- ["4"] = { "select_4", mode = { "n", "i" } },
+                            -- ["5"] = { "select_5", mode = { "n", "i" } },
+                            -- ["6"] = { "select_6", mode = { "n", "i" } },
+                            -- ["7"] = { "select_7", mode = { "n", "i" } },
+                            -- ["8"] = { "select_8", mode = { "n", "i" } },
+                            -- ["9"] = { "select_9", mode = { "n", "i" } },
+                            -- ["0"] = { "select_0", mode = { "n", "i" } },
                             ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
                             ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
                             ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },
