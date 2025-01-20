197c2e7d5b9dd75c56e933150f6a3f0f4802908d
Peter Tri Ho <mail@petertriho.com>
2025-01-20 14:33:37 +1100

    feat(nvim): add keymap to clear fidget

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/fidget.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/fidget.lua
@@ -1,6 +1,9 @@
 return {
     "j-hui/fidget.nvim",
     event = "VeryLazy",
+    keys = {
+        { "<leader>of", "<CMD>Fidget clear<CR>", desc = "Fidget Clear" },
+    },
     opts = {
         integration = {
             ["nvim-tree"] = {
