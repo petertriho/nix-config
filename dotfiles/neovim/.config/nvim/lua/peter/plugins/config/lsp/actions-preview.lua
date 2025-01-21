be067e00c7299b66017d4ef63b63be11393c7b62
Peter Tri Ho <mail@petertriho.com>
2025-01-21 12:29:31 +1100

    fix(nvim): errors when selecting an action in `actions-preview`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/actions-preview.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/lsp/actions-preview.lua
@@ -12,6 +12,7 @@ return {
     },
     config = function()
         local actions = require("telescope.actions")
+        local state = require("telescope.actions.state")
         local select = function(n)
             return function(bufnr)
                 actions.move_to_top(bufnr)
@@ -24,13 +25,26 @@ return {
 
         require("actions-preview").setup({
             telescope = vim.tbl_extend("force", require("telescope.themes").get_dropdown(), {
-                attach_mappings = function(_, map)
+                attach_mappings = function(prompt_bufnr, map)
+                    actions.select_default:replace(function()
+                        local selection = state.get_selected_entry()
+                        actions.close(prompt_bufnr)
+                        if not selection then
+                            return
+                        end
+
+                        selection.value.action:apply()
+                    end)
+
                     for i = 1, 9 do
                         map({ "i", "n" }, tostring(i), select(i))
                     end
                     map({ "i", "n" }, "0", select(10))
+
                     return true
                 end,
+                make_value = nil,
+                make_make_display = nil,
             }),
         })
     end,
