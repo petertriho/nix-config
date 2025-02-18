d54115ecd66f08a3ee60452354c5b5adcd18a4ff
Peter Tri Ho <mail@petertriho.com>
2025-02-19 10:36:42 +1100

    chore(nix): update `flake.lock`

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
