a047ffc99af3bc8561a13b2841b0d37f4928ed91
Peter Tri Ho <mail@petertriho.com>
2025-02-20 09:35:38 +1100

    chore(nix): update `kanata-macos` to `v1.8.0`

--- pkgs/kanata-macos/default.nix
+++ pkgs/kanata-macos/default.nix
@@ -5,19 +5,19 @@
 }:
 let
   asset = if stdenv.isAarch64 then "kanata_macos_arm64" else "kanata_macos_x86_64";
-  sha256 =
+  hash =
     if stdenv.isAarch64 then
-      "1fmv60g4r3ij1m6xa1bxsd93whjc1l69bd96qrx7dy96n27bii60"
+      "sha256-oHIpb1Hvi3gJUYnYJWXGs1QPoHerdWCA1+bHjG4QAQ4="
     else
-      "1rhvv4ps7vs9yq8xwybn0ivw7wmvm09j5nrx0d5yj2i6r3cgs9zq";
+      "sha256-5p7KR0TWmCnDjKR0r2zT7q6Au8S6iNr5xgtitqBBwZ8=";
 in
 stdenv.mkDerivation rec {
   pname = "kanata-macos";
-  version = "v1.8.0-prerelease-1";
+  version = "v1.8.0";
 
   src = fetchurl {
     url = "https://github.com/jtroo/kanata/releases/download/${version}/${asset}";
-    inherit sha256;
+    inherit hash;
   };
 
   dontUnpack = true;
