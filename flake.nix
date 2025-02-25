80c137aeade4c10c5ee57fc9b1ddc0f225e719e6
Peter Tri Ho <mail@petertriho.com>
2025-02-25 16:50:33 +1100

    feat(nix): move cachix cache to `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "Nix Config";
 
+  nixConfig = {
+    extra-experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
--- systems/base/default.nix
+++ systems/base/default.nix
@@ -26,12 +26,6 @@
         "root"
         (if pkgs.stdenv.isLinux then "@wheel" else "@admin")
       ];
-      substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     };
   };
 
