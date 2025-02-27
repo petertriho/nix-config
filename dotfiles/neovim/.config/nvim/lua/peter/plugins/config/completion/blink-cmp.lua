79b7b26546ce807cac8c01abdaf0cef848fe9b74
Peter Tri Ho <mail@petertriho.com>
2025-02-27 14:50:45 +1100

    revert(nvim): default completion behavior for `cmdline`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/blink-cmp.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/completion/blink-cmp.lua
@@ -70,13 +70,6 @@ return {
                 end,
             },
         },
-        cmdline = {
-            keymap = {
-                preset = "default",
-                ["<Tab>"] = { "select_next", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "fallback" },
-            },
-        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = "mono",
