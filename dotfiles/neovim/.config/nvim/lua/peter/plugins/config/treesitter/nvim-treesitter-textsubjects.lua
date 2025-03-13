3c39e05ffbe24ab0281461e3efbcbab5709d962d
Peter Tri Ho <mail@petertriho.com>
2025-03-13 16:46:21 +1100

    fix(nvim): update `nvim-treesitter-textsubjects` config

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/treesitter/nvim-treesitter-textsubjects.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/treesitter/nvim-treesitter-textsubjects.lua
@@ -2,15 +2,12 @@ return {
     "RRethy/nvim-treesitter-textsubjects",
     event = "User LoadedNvimTreesitter",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            textsubjects = {
-                enable = true,
-                prev_selection = "<BS>",
-                keymaps = {
-                    ["."] = { "textsubjects-smart", desc = "TS Smart" },
-                    ["<CR>"] = { "textsubjects-container-outer", desc = "TS Outer" },
-                    ["i<CR>"] = { "textsubjects-container-inner", desc = "TS Inner" },
-                },
+        require("nvim-treesitter-textsubjects").configure({
+            prev_selection = "<BS>",
+            keymaps = {
+                ["."] = { "textsubjects-smart", desc = "TS Smart" },
+                ["<CR>"] = { "textsubjects-container-outer", desc = "TS Outer" },
+                ["i<CR>"] = { "textsubjects-container-inner", desc = "TS Inner" },
             },
         })
     end,
