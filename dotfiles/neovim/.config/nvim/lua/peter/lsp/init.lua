3e07eb738666c6606e5f61c3aec412de20b87406
Peter Tri Ho <mail@petertriho.com>
2025-04-22 22:00:23 +1000

    WIP(nvim): add `heirline`

--- dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
@@ -267,10 +267,10 @@ M.setup = function()
         severity_sort = true,
         signs = {
             text = {
-                [vim.diagnostic.severity.ERROR] = "",
-                [vim.diagnostic.severity.WARN] = "!",
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
                 [vim.diagnostic.severity.HINT] = "󰌶",
-                [vim.diagnostic.severity.INFO] = "i",
+                [vim.diagnostic.severity.INFO] = "",
             },
         },
     })
