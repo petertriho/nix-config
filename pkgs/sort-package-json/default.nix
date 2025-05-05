7cf7f88c17e96c2aed768dc450701460c7faeb22
Peter Tri Ho <mail@petertriho.com>
2025-05-05 19:15:57 +1000

    chore(nix): update `flake.lock`

--- pkgs/sort-package-json/default.nix
+++ pkgs/sort-package-json/default.nix
@@ -4,12 +4,12 @@
 }:
 buildNpmPackage {
   pname = "sort-package-json";
-  version = "unstable-2025-04-29";
+  version = "unstable-2025-05-04";
   src = fetchFromGitHub {
     owner = "keithamus";
     repo = "sort-package-json";
-    rev = "9f1101b0dc82276c0b72052bac2117ae570b5be4";
-    sha256 = "089z9hj18n9qvb6z8x2vmhwjc629qx5r1x3fi8inmxrxmz76zfjv";
+    rev = "27e4b7bf4d1357c43c147861b76fcf0f79c661d5";
+    sha256 = "1r5qwz80z895lm5wp3n6hl9vkkbpr6c3zmqfchrrq5sx62b4wdbq";
   };
   npmDepsHash = "sha256-vPizvY1Wuv1svGiWwQeqUI3UYdu/F4X0vQGdJIyHU0Q=";
   dontNpmBuild = true;
