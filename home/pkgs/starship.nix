d9de6055423d08856105c4484437d24aba408ebc
Peter Tri Ho <mail@petertriho.com>
2025-05-20 10:00:25 +1000

    fix(starship): git timeout messages

--- home/pkgs/starship.nix
+++ home/pkgs/starship.nix
@@ -4,7 +4,12 @@
   ...
 }:
 {
-  home.packages = with pkgs; [ starship ];
+  home = {
+    packages = with pkgs; [ starship ];
+    sessionVariables = {
+      STARSHIP_CONFIG = "${config.home.homeDirectory}/.config/starship.toml";
+    };
+  };
 
   xdg.configFile."starship.toml".source = config.lib.meta.mkDotfilesSymlink "starship/starship.toml";
 }
