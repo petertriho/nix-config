4e04048f04d756ac103bd51c7caf20c87b90b407
Peter Tri Ho <mail@petertriho.com>
2025-05-25 12:22:57 +1000

    chore(nix): update `flake.lock`

--- pkgs/tmux-plugins/tinted/default.nix
+++ pkgs/tmux-plugins/tinted/default.nix
@@ -5,11 +5,11 @@
 }:
 tmuxPlugins.mkTmuxPlugin {
   pluginName = "tmuxcolors";
-  version = "unstable-2025-04-20";
+  version = "unstable-2025-05-25";
   src = fetchFromGitHub {
     owner = "tinted-theming";
     repo = "tinted-tmux";
-    rev = "e009f18a01182b63559fb28f1c786eb027c3dee9";
-    sha256 = "1773r4v4v8gj9fgxi3bxpfwyldh3xzdrmx6h4c4hkak9fyf7xmdr";
+    rev = "e96128685252e693f6943160cc6afbe4650c7d24";
+    sha256 = "1hai8q5gyyzsbqqc79fckkbyaxsvlkpjwv8cm6kh8zn4zxrdrx5q";
   };
 }
