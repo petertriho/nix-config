9ada2334613c76c81bb02045ba4a9eaf61809327
Peter Tri Ho <mail@petertriho.com>
2025-06-08 17:19:02 +1000

    chore(nix): update `flake.lock`

--- pkgs/sort-package-json/default.nix
+++ pkgs/sort-package-json/default.nix
@@ -4,14 +4,14 @@
 }:
 buildNpmPackage {
   pname = "sort-package-json";
-  version = "unstable-2025-05-08";
+  version = "unstable-2025-06-08";
   src = fetchFromGitHub {
     owner = "keithamus";
     repo = "sort-package-json";
-    rev = "aa6774ad937feb83178c8bc981f08305e1d22b5c";
-    sha256 = "0wspgbz27qkipc707x3m4a6xkzc46ppmrfrvfq1xfl8y4049qx9i";
+    rev = "1c142c61f4cd370fc3b17f150927ed4d82efa6f3";
+    sha256 = "14ijyxfd2vhi56ypnb0kgjw1ksjlh3ggwycrlc02yx6q90xgbz4q";
   };
-  npmDepsHash = "sha256-vPizvY1Wuv1svGiWwQeqUI3UYdu/F4X0vQGdJIyHU0Q=";
+  npmDepsHash = "sha256-KYyGkS6b+8ruUBbgs8PTZAA11MgwZK7Lsu6qV+KmOTE=";
   dontNpmBuild = true;
   postInstall = ''
     find -L $out -type l -print -delete
--- pkgs/tmux-plugins/tinted/default.nix
+++ pkgs/tmux-plugins/tinted/default.nix
@@ -5,11 +5,11 @@
 }:
 tmuxPlugins.mkTmuxPlugin {
   pluginName = "tmuxcolors";
-  version = "unstable-2025-06-01";
+  version = "unstable-2025-06-08";
   src = fetchFromGitHub {
     owner = "tinted-theming";
     repo = "tinted-tmux";
-    rev = "57d5f9683ff9a3b590643beeaf0364da819aedda";
-    sha256 = "0gk7wpxb81ggxa5jmd53ir2fm5hwqmg72kinb9wq65rvp3ckahrq";
+    rev = "e77f752cfbc2db3902d8d36e0c6e71979e3e556d";
+    sha256 = "0awclrgr79n9pkdjhlg155nx59h2hzxdhff7xn9szf48xs4m2giy";
   };
 }
