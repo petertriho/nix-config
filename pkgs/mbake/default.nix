4d6b654e6fc1f012e13300b71d432bcea2472368
Peter Tri Ho <mail@petertriho.com>
2025-06-23 19:32:10 +1000

    chore(nix): update `flake.lock`

--- pkgs/mbake/default.nix
+++ pkgs/mbake/default.nix
@@ -6,11 +6,11 @@
 with python3Packages;
 buildPythonApplication rec {
   pname = "mbake";
-  version = "1.1.3";
+  version = "1.2.1";
   format = "pyproject";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-hc6KvDE9EVVkc/VfiTsGfGrdJCUOam1SfeTW3XNCOF0=";
+    sha256 = "sha256-ssXJwUPRM57xBPaxvfNrOq1xsNL7qkGYzsSgtUflWT4=";
   };
   build-system = [
     hatchling
