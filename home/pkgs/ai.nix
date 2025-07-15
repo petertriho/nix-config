1dd1d8c5bed23d32bbf7669775316965fa8dca02
Peter Tri Ho <mail@petertriho.com>
2025-07-15 11:37:41 +1000

    feat(opencode): add initial configuration

--- home/pkgs/ai.nix
+++ home/pkgs/ai.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [ opencode ];
+
+  xdg.configFile."opencode/opencode.json".source =
+    config.lib.meta.mkDotfilesSymlink "opencode/.config/opencode.json";
 }
