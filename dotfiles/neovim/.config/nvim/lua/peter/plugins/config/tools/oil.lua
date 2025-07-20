c8bb805428fbdfea485c3a91b686f8c4c26d12d9
Peter Tri Ho <mail@petertriho.com>
2025-07-20 22:06:12 +1000

    refactor(nvim): replace `nvim-lsp-file-operations` with `snacks`

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/oil.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/oil.lua
@@ -43,4 +43,16 @@ return {
             show_hidden = true,
         },
     },
+    config = function(_, opts)
+        require("oil").setup(opts)
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "OilActionsPost",
+            callback = function(event)
+                if event.data.actions.type == "move" then
+                    require("snacks").rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+                end
+            end,
+        })
+    end,
 }
