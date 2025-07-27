a81adb94f1b73c9749c5ca694d32d61c80f41504
Peter Tri Ho <mail@petertriho.com>
2025-07-27 20:08:40 +1000

    refactor(nvim): move `nvim-mundo` to separate repo

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/nvim-mundo.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/nvim-mundo.lua
@@ -1,6 +1,5 @@
 return {
-    -- Custom Lua rewrite of vim-mundo with improved diff and tree visualization
-    dir = "~/.config/nvim/plugins/nvim-mundo",
+    "petertriho/nvim-mundo",
     cmd = {
         "MundoToggle",
         "MundoShow",
