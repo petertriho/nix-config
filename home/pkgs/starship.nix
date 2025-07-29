c8819779b16c06953827fa31f8fd09de371d8662
Peter Tri Ho <mail@petertriho.com>
2025-07-29 10:15:26 +1000

    feat(starship): separate starship config for tmux

--- home/pkgs/starship.nix
+++ home/pkgs/starship.nix
@@ -7,9 +7,11 @@
   home = {
     packages = with pkgs; [ starship ];
     sessionVariables = {
-      STARSHIP_CONFIG = "${config.home.homeDirectory}/.config/starship.toml";
+      STARSHIP_CONFIG = "${config.home.homeDirectory}/.config/starship/starship.toml";
     };
   };
 
-  xdg.configFile."starship.toml".source = config.lib.meta.mkDotfilesSymlink "starship/starship.toml";
+  xdg.configFile."starship/starship.toml".source = config.lib.meta.mkDotfilesSymlink "starship/.config/starship/starship.toml";
+  xdg.configFile."starship/starship-tmux.toml".source =
+    config.lib.meta.mkDotfilesSymlink "starship/.config/starship/starship-tmux.toml";
 }
