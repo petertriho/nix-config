0716b74c21cd6cdc7e879a1400681a31e6cc8967
Peter Tri Ho <mail@petertriho.com>
2025-08-03 17:51:31 +1000

    feat(nvim): switch to forked typst-preview.nvim plugin

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/typst-preview.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/typst-preview.lua
@@ -1,5 +1,8 @@
 return {
-    "chomosuke/typst-preview.nvim",
+    -- TODO: revert to original plugin when merged
+    -- "chomosuke/typst-preview.nvim",
+    "wisp3rwind/typst-preview.nvim",
+    branch = "add_lsp",
     ft = "typst",
     opts = {},
 }
