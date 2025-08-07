42dde5e83d90e706124bec28b3f6f0aca4e4eca6
Peter Tri Ho <mail@petertriho.com>
2025-08-07 23:34:08 +1000

    fix(nvim): change info diagnostic icon

--- dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/lsp/init.lua
@@ -342,7 +342,7 @@ M.setup = function()
                 [vim.diagnostic.severity.ERROR] = "",
                 [vim.diagnostic.severity.WARN] = "",
                 [vim.diagnostic.severity.HINT] = "󰌶",
-                [vim.diagnostic.severity.INFO] = "",
+                [vim.diagnostic.severity.INFO] = "",
             },
         },
     })
