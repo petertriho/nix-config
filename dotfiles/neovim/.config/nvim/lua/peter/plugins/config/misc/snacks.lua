12fe901e2929a465ea3ae27b0a8643828ecc290e
Peter Tri Ho <mail@petertriho.com>
2025-08-10 21:03:37 +1000

    feat(nvim): integrtae flash into snacks pickers
    simplify select function

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/snacks.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/misc/snacks.lua
@@ -31,14 +31,30 @@ return {
 
         local select = function(n)
             return function(picker)
-                picker:action("list_top")
-
-                if n > 1 then
-                    picker.list:move(n - 1)
-                end
-
+                local idx = picker.list:row2idx(n - 1)
+                picker.list:_move(idx, true, true)
                 picker:action("confirm")
             end
+        end
+
+        local flash = function(picker)
+            require("flash").jump({
+                pattern = "^",
+                label = { after = { 0, 0 } },
+                search = {
+                    mode = "search",
+                    exclude = {
+                        function(win)
+                            return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                        end,
+                    },
+                    multi_window = true,
+                },
+                action = function(match)
+                    local idx = picker.list:row2idx(match.pos[1])
+                    picker.list:_move(idx, true, true)
+                end,
+            })
         end
 
         local exclude = vim.opt.wildignore:get()
@@ -152,10 +168,13 @@ return {
                             ["<c-b>"] = { "list_scroll_up", mode = { "n", "i" } },
                             ["<c-f>"] = { "list_scroll_down", mode = { "n", "i" } },
                             ["<c-l>"] = { "toggle_preview", mode = { "n", "i" } },
+                            ["<c-s>"] = { "flash", mode = { "n", "i" } },
+                            ["s"] = { "flash" },
                         },
                     },
                 },
                 actions = {
+                    flash = flash,
                     select_1 = select(1),
                     select_2 = select(2),
                     select_3 = select(3),
