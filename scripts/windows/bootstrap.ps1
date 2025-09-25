e7e230e527fc7f629c5c7580f4bfd646b1894e59
Peter Tri Ho <mail@petertriho.com>
2025-09-25 23:15:26 +1000

    feat(boostrap): add windows bootstrap scripts

---
+++ scripts/windows/bootstrap.ps1
@@ -0,0 +1,58 @@
+# Check list
+# - Drivers
+# - Sophia app https://github.com/Sophia-Community/SophiApp
+# - Winget
+# - Scoop
+# - WSL
+# - excludeWSL.ps1
+# - nix-config
+
+# Winget
+winget install --source winget --id Ablaze.Floorp
+winget install --source winget --id AltSnap.AltSnap
+winget install --source winget --id Betterbird.Betterbird
+winget install --source winget --id Discord.Discord
+winget install --source winget --id Giorgiotani.Peazip
+winget install --source winget --id Google.Chrome
+winget install --source winget --id MartiCliment.UniGetUI
+winget install --source winget --id Microsoft.PowerToys
+winget install --source winget --id Mozilla.Firefox
+winget install --source winget --id Neovide.Neovide
+winget install --source winget --id Nextcloud.NextcloudDesktop
+winget install --source winget --id RandyRants.SharpKeys
+winget install --source winget --id SumatraPDF.SumatraPDF
+winget install --source winget --id Valve.Steam
+winget install --source winget --id VideoLAN.VLC
+winget install --source winget --id voidtools.Everything
+winget install --source winget --id WinSCP.WinSCP
+winget install --source winget --id WireGuard.WireGuard
+winget install --source winget --id wez.wezterm
+
+# Scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+irm get.scoop.sh | iex
+scoop install 7zip aria2 curl dark git neovim sudo win32yank
+scoop bucket add nerd-fonts
+scoop bucket add extras
+sudo scoop install --global JetBrainsMono-NF JetBrainsMono-NF-Mono JetBrainsMono-NF-Propo
+
+# WSL
+wsl --install --no-distribution
+
+# Download and install NixOS-WSL (https://github.com/nix-community/NixOS-WSL)
+$releaseUrl = "https://api.github.com/repos/nix-community/NixOS-WSL/releases/latest"
+$release = Invoke-RestMethod -Uri $releaseUrl
+$asset = $release.assets | Where-Object { $_.name -eq "nixos.wsl" }
+$downloadUrl = $asset.browser_download_url
+$outputPath = Join-Path $env:USERPROFILE "Downloads\nixos.wsl"
+
+Write-Host "Downloading NixOS-WSL..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputPath
+
+Write-Host "Installing NixOS-WSL..."
+Start-Process -FilePath $outputPath -Wait
+
+Write-Host "NixOS-WSL installed. You can now run: wsl -d NixOS"
+
+# Change username and networking.hostName = "WSL" before running the nix-config bootstrap script
+# https://nix-community.github.io/NixOS-WSL/how-to/change-username.html
---
+++ scripts/windows/excludeWSL.ps1
@@ -0,0 +1,71 @@
+# Modified from https://gist.github.com/dkorobtsov/963f3b90418e51d12aecb1eaf6106958
+# PowerShell script to add Windows Defender exclusions for WSL2 performance issues
+#
+# For context please read this thread:
+# https://github.com/microsoft/WSL/issues/8995
+#
+# How to use?
+# - Run PowerShell as Administrator: Search for "PowerShell" in the Start menu, right-click on it, and choose "Run as administrator".
+
+# Check if running as administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+if (-not $isAdmin) {
+    Write-Host "This script must be run as Administrator. Exiting."
+    return
+}
+
+# Define folders to exclude, adjust if needed
+$foldersToExclude = @(
+    "C:\Program Files\Docker",
+    "\\wsl$\NixOS",
+    "\\wsl.localhost\NixOS",
+    "\\wsl$\docker-desktop",
+    "\\wsl.localhost\docker-desktop"
+)
+
+# Define file types to exclude, adjust if needed
+$fileTypesToExclude = @(
+    "vhd",
+    "vhdx"
+)
+
+# Define processes to exclude, adjust if needed
+$processesToExclude = @(
+    "docker.exe",
+    "com.docker.*.*",
+    "Desktop Docker.exe",
+    "wsl.exe",
+    "wslhost.exe",
+    "vmmemWSL"
+)
+
+# Add Firewall Rule for WSL
+# For details please read official documentation:
+# https://www.jetbrains.com/help/idea/how-to-use-wsl-development-environment-in-product.html#debugging_system_settings
+Write-Host "Adding firewall rules for WSL. This step may take a few minutes..."
+try {
+    New-NetFirewallRule -DisplayName "WSL" -Direction Inbound  -InterfaceAlias "vEthernet (WSL)"  -Action Allow
+    Get-NetFirewallProfile -Name Public | Get-NetFirewallRule | Where-Object DisplayName -ILike "$($selectedIDE)*" | Disable-NetFirewallRule
+} catch {
+    Write-Host "Error adding firewall rule: $_"
+}
+
+# Add folder exclusions
+Write-Host "Adding folder exclusions..."
+foreach ($folder in $foldersToExclude) {
+    Add-MpPreference -ExclusionPath $folder
+}
+
+# Add file type exclusions
+Write-Host "Adding file type exclusions..."
+foreach ($fileType in $fileTypesToExclude) {
+    Add-MpPreference -ExclusionExtension $fileType
+}
+
+# Add process exclusions
+Write-Host "Adding process exclusions..."
+foreach ($process in $processesToExclude) {
+    Add-MpPreference -ExclusionProcess $process
+}
+
+Write-Host "Script execution completed."
