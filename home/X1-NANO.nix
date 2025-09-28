6e9bd400a7df6bc98858a59745588bb602ea569c
Peter Tri Ho <mail@petertriho.com>
2025-09-28 18:52:47 +1000

    feat(X1-NANO): add hyprland monitor settings

--- home/X1-NANO.nix
+++ home/X1-NANO.nix
@@ -8,4 +8,14 @@
       COPILOT_MODEL = "gpt-4.1";
     };
   };
+  wayland.windowManager.hyprland = {
+    settings = {
+      monitorv2 = {
+        output = "desc:Chimei Innolux Corporation 0x1301";
+        mode = "preferred";
+        position = "auto";
+        scale = 1.50;
+      };
+    };
+  };
 }
