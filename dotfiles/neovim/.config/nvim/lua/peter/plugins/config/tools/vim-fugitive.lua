fa4a802da808266b61ef2ecd275bfe121f333cc7
Peter Tri Ho <mail@petertriho.com>
2025-10-17 14:08:31 +1100

    fix(nvim): change git diffget keymaps to dgl/dgr

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/vim-fugitive.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/vim-fugitive.lua
@@ -10,8 +10,8 @@ return {
         { "<leader>j", "<CMD>ToggleGitStatus<CR>", desc = "Git Status" },
     },
     init = function()
-        vim.keymap.set("n", "g[", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
-        vim.keymap.set("n", "g]", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
+        vim.keymap.set("n", "dgl", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
+        vim.keymap.set("n", "dgr", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
 
         vim.api.nvim_create_user_command(
             "ToggleGitStatus",
