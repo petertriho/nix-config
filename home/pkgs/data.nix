6977e5213650e8153364b1cf0450043bbfe56eee
Peter Tri Ho <mail@petertriho.com>
2025-10-27 17:56:12 +1100

    feat(nix): add `faker`

--- home/pkgs/data.nix
+++ home/pkgs/data.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     csvkit # command-line CSV toolkit
     duckdb # SQL OLAP db management system
+    python3Packages.faker # fake data generation library
     fx # JSON viewer
     gron # make JSON greppable
     python3Packages.pgcli # PostgreSQL CLI with autocompletion
