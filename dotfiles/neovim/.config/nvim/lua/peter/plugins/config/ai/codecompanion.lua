c1f824206cccbf9f2fabd60a7dab5ab0ed091d56
Peter Tri Ho <mail@petertriho.com>
2025-12-16 15:56:58 +1100

    revert(nvim): use default opencode adapter

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ai/codecompanion.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ai/codecompanion.lua
@@ -44,56 +44,6 @@ return {
                             },
                         })
                     end,
-                    opencode = function()
-                        return {
-                            name = "opencode",
-                            formatted_name = "OpenCode",
-                            type = "acp",
-                            roles = {
-                                llm = "assistant",
-                                user = "user",
-                            },
-                            opts = {
-                                vision = false,
-                            },
-                            commands = {
-                                default = {
-                                    "opencode",
-                                    "acp",
-                                },
-                            },
-                            defaults = {
-                                mcpServers = {},
-                                timeout = 20000,
-                            },
-                            parameters = {
-                                protocolVersion = 1,
-                                clientCapabilities = {
-                                    fs = { readTextFile = true, writeTextFile = true },
-                                },
-                                clientInfo = {
-                                    name = "CodeCompanion.nvim",
-                                    version = "1.0.0",
-                                },
-                            },
-                            handlers = {
-                                setup = function(self)
-                                    return true
-                                end,
-                                auth = function(self)
-                                    return true
-                                end,
-                                form_messages = function(self, messages, capabilities)
-                                    return require("codecompanion.adapters.acp.helpers").form_messages(
-                                        self,
-                                        messages,
-                                        capabilities
-                                    )
-                                end,
-                                on_exit = function(self, code) end,
-                            },
-                        }
-                    end,
                 },
             },
         })
