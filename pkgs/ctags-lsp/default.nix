05dd2b39708f3fb7f1d8e9bffdc3ab631fe19c81
Peter Tri Ho <mail@petertriho.com>
2026-01-15 11:03:01 +1100

    fix(nix): build for `ctags-lsp`

--- pkgs/ctags-lsp/default.nix
+++ pkgs/ctags-lsp/default.nix
@@ -17,8 +17,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "netmute";
     repo = "ctags-lsp";
-    tag = "v${version}";
-    hash = "sha256-8cknVcXIuV7mmRMm87jn2l3qrfaY3CGzCZ0VW5Vb9xk=";
+    rev = "34fe1992683eeb3e4ea32d299992d13d8501a46d";
+    sha256 = "06gpbfamn5cx16rj3p4qysnylpfswywg69hkk7k5xff8qmajgjgi";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -32,22 +32,22 @@ buildGoModule rec {
   postInstall = ''
     wrapProgram $out/bin/ctags-lsp \
         --suffix PATH :${
-        lib.makeBinPath [
-          universal-ctags
-          git
-          jujutsu
-        ]
-      }
+          lib.makeBinPath [
+            universal-ctags
+            git
+            jujutsu
+          ]
+        }
 
 '';
 
-  doInstallCheck = true;
-  nativeInstallCheckInputs = [ versionCheckHook ];
-
-  passthru.updateScript = nix-update-script { };
+  # doInstallCheck = true;
+  # nativeInstallCheckInputs = [ versionCheckHook ];
+  #
+  # passthru.updateScript = nix-update-script { };
 
   meta = {
-    changelog = "https://github.com/netmute/ctags-lsp/releases/tag/v${version}";
+    # changelog = "https://github.com/netmute/ctags-lsp/releases/tag/v${version}";
     description = "LSP implementation using universal-ctags as backend";
     homepage = "https://github.com/netmute/ctags-lsp";
     license = lib.licenses.mit;
