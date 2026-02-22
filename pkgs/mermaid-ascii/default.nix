643984729bebb2c2435448e72f49bbec297d0cdd
Peter Tri Ho <mail@petertriho.com>
2026-02-22 12:22:35 +1100

    chore(nix): update `flake.lock`

--- pkgs/mermaid-ascii/default.nix
+++ pkgs/mermaid-ascii/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "mermaid-ascii";
-  version = "1.1.0-unstable-2026-02-01";
+  version = "1.1.0-unstable-2026-02-21";
 
   src = fetchFromGitHub {
     owner = "AlexanderGrooff";
     repo = "mermaid-ascii";
-    rev = "2955c2e36e05e15ab3264a3896ae027de3aba1db";
-    hash = "sha256-2dMpUxzqYqfXJvBYu0udzt66GNLDvkp6eJZLHrovm4I=";
+    rev = "b5d02c35decfab219bd6cc79094b117d8079bd63";
+    hash = "sha256-bFqlMwBfvm75aKiUU1GZadGtMWppCabwdvguMzK/KBo=";
   };
 
   vendorHash = "sha256-aB9sbTtlHbptM2995jizGFtSmEIg3i8zWkXz1zzbIek=";
