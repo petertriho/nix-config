b20c9ccd2e3ae80fad803e6ab3f131b873a50dca
Peter Tri Ho <mail@petertriho.com>
2026-03-06 19:40:55 +1100

    feat(nix): add support for pinchtab to use chromium through wslg

--- home/WSL.nix
+++ home/WSL.nix
@@ -1,8 +1,13 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [ ./base.nix ];
   home = {
+    packages = with pkgs; [
+      ungoogled-chromium
+    ];
     sessionVariables = rec {
+      CHROME_BINARY = "${pkgs.ungoogled-chromium}/bin/chromium";
+      CHROME_FLAGS = "--no-sandbox";
       COPILOT_MODEL = "gpt-5-mini";
       SCRATCH_PATH = "/mnt/c/Users/peter/Nextcloud/Documents/Scratch";
       OBSIDIAN_PATH = "/mnt/c/Users/peter/Nextcloud/Documents/Obsidian";
