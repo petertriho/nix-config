e6ac2be05dd88389a7120ffff37fe94428185abf
Peter Tri Ho <mail@petertriho.com>
2026-03-07 11:41:41 +1100

    chore(nix): update `flake.lock`

--- pkgs/pinchtab/default.nix
+++ pkgs/pinchtab/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "pinchtab";
-  version = "0.7.7-unstable-2026-03-05";
+  version = "0.7.8-unstable-2026-03-06";
 
   src = fetchFromGitHub {
     owner = "pinchtab";
     repo = "pinchtab";
-    rev = "02c2396e2c752f71837d5603ddc1f1e11f7ec057";
-    sha256 = "sha256-VijwJScQj5uEC0KfVTMae7JmQHXOnwA9aWyfSHxbBw8=";
+    rev = "cd5f42aa6c813c5e976d388d2a029bbf31625a4f";
+    sha256 = "sha256-szQ/pWVhkk9jAUZIT3/vF1JZhtuoFPAzjeHUQ3XQgvM=";
   };
 
   vendorHash = "sha256-l6dOEWaG4w8WoS4RvAa5AD0ynmXThJnZLUMzFvATAlE=";
--- pkgs/plannotator/default.nix
+++ pkgs/plannotator/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "plannotator";
-  version = "0.11.0-unstable-2026-03-06";
+  version = "0.11.1-unstable-2026-03-06";
 
   src = fetchFromGitHub {
     owner = "backnotprop";
     repo = "plannotator";
-    rev = "3f73ddaead3a0fc15411598703458d7be98a6a02";
-    sha256 = "sha256-OE1Jv9W9l7LlqoSoVy/o5DvGdCo8li3L5mxAv6Nz2c4=";
+    rev = "0ce299dd92cc01ae5e350bedeacec5670e1b801f";
+    sha256 = "sha256-O9lV4hh8fifaObvlWpX31cjy7BEEN3OpGVZm8PiWYOU=";
   };
 
   dontBuild = true;
