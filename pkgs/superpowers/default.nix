ddfd1d2cc20a340af437574cb8483f6f7718117c
Peter Tri Ho <mail@petertriho.com>
2026-03-11 14:57:00 +1100

    chore(nix): update `superpowers`

--- pkgs/superpowers/default.nix
+++ pkgs/superpowers/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "superpowers";
-  version = "unstable-2026-03-09";
+  version = "unstable-2026-03-10";
 
   src = fetchFromGitHub {
     owner = "obra";
     repo = "superpowers";
-    rev = "33e55e60b2efcb69509bc233dfc128112012b2c8";
-    sha256 = "1bxkx9345szv2f0fzxak8nb5657yvk00j381zjbgjv902wv1bvs5";
+    rev = "5ef73d25b71e69e63a74eae359fb26c7a6ac8fc0";
+    sha256 = "0yic5rq8fdrdr8vr5zl56cg2yxjxyhrc17ks2axn14blp426j42v";
   };
 
   dontBuild = true;
