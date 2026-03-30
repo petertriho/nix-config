315241141bc2fc2e583ed41896942d61b1df25e2
Peter Tri Ho <mail@petertriho.com>
2026-03-30 15:53:12 +1100

    revert(nix): use default model for claude

--- modules/home-manager/programs/claude-code.nix
+++ modules/home-manager/programs/claude-code.nix
@@ -22,8 +22,8 @@ in
         ANTHROPIC_BASE_URL = "https://api.z.ai/api/anthropic";
         API_TIMEOUT_MS = "3000000";
         ANTHROPIC_DEFAULT_HAIKU_MODEL = "glm-5-turbo";
-        ANTHROPIC_DEFAULT_SONNET_MODEL = "glm-4.7";
-        ANTHROPIC_DEFAULT_OPUS_MODEL = "glm-5.1";
+        ANTHROPIC_DEFAULT_SONNET_MODEL = "glm-5.1";
+        ANTHROPIC_DEFAULT_OPUS_MODEL = "glm-4.7";
       };
     })
   ];
