48c64d42652c9eb639ad02c2e6f514537dd7885b
Peter Tri Ho <mail@petertriho.com>
2026-03-30 15:13:03 +1100

    fix(nix): opencode config

--- home/pkgs/ai.nix
+++ home/pkgs/ai.nix
@@ -60,7 +60,7 @@ let
       inherit extensions;
     };
   opencodeLspConfig = lib.mapAttrs toOpencodeLsp lspServers // {
-    lua-ls.disabled = false;
+    lua-ls.disabled = true;
     pyright.disabled = true;
     typescript.disabled = true;
   };
