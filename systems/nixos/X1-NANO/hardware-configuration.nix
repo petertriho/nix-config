c099b49f90871f5daafe02b0b0512d844ec5b3aa
Peter Tri Ho <mail@petertriho.com>
2026-04-25 19:30:53 +1000

    perf(nix): reduce max cpu for x1-nano

--- systems/nixos/X1-NANO/hardware-configuration.nix
+++ systems/nixos/X1-NANO/hardware-configuration.nix
@@ -61,7 +61,7 @@
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MAX_PERF_ON_AC = 75;
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 35;
 
