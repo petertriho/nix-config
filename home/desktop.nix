4d06a3725689d1d6c354e24d15e221f06174baf5
Peter Tri Ho <mail@petertriho.com>
2026-04-26 20:20:49 +1000

    fix(nix): add gtk theme and dark mode

--- home/desktop.nix
+++ home/desktop.nix
@@ -23,5 +23,20 @@
   ];
   programs.claude-code.zai.enable = true;
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    gtk4.theme = config.gtk.theme;
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
   xdg.configFile."mimeapps.list".source = config.lib.meta.mkDotfilesSymlink "mimeapps/.config/mimeapps.list";
 }
