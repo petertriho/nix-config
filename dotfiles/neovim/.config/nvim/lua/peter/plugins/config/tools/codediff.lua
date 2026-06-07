09690f78609aab203c61582e90479cf3bd73b78a
Peter Tri Ho <mail@petertriho.com>
2026-06-07 16:32:54 +1000

    feat(nvim): add keymaps for codediff

--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/codediff.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/tools/codediff.lua
@@ -2,4 +2,8 @@ return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = { "CodeDiff" },
+    keys = {
+        { "<leader>gD", "<CMD>CodeDiff<CR>", desc = "CodeDiff" },
+        { "<leader>gH", "<CMD>CodeDiff history %<CR>", desc = "History File" },
+    },
 }
