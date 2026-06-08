235b9449817a8e89e11b09ce291eb60413531bd7
Peter Tri Ho <mail@petertriho.com>
2026-06-08 16:20:37 +1000

    fix(nix): vicinae settings

--- home/pkgs/vicinae.nix
+++ home/pkgs/vicinae.nix
@@ -7,10 +7,18 @@
       autoStart = true;
     };
     settings = {
+      activate_on_single_click = true;
       close_on_focus_loss = true;
-      faviconService = "twenty";
-      font.size = 11;
-      popToRootOnClose = true;
+      favicon_service = "twenty";
+      font.normal.size = 11;
+      launcher_window = {
+        opacity = 1;
+        client_side_decorations = {
+          enabled = true;
+          rounding = 10;
+        };
+      };
+      pop_to_root_on_close = true;
       providers.applications.preferences.defaultAction = "launch";
       providers.power.entrypoints = {
         "power-off".preferences.confirm = false;
@@ -18,13 +26,11 @@
         logout.preferences.confirm = false;
         logout.preferences.customProgram = "niri msg action quit";
       };
-      rootSearch.searchFiles = false;
+      search_files_in_root = true;
       telemetry.system_info = false;
-      theme.name = "vicinae-dark";
-      window = {
-        csd = true;
-        opacity = 0.95;
-        rounding = 10;
+      theme = {
+        light.name = "vicinae-dark";
+        dark.name = "vicinae-dark";
       };
     };
   };
