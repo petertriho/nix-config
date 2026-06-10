78ef3d35d5593247d8a5b72150521a119f05c51c
Peter Tri Ho <mail@petertriho.com>
2026-06-10 12:07:09 +1000

    feat(nix): add `etcd` and `grpc-tools`

--- home/pkgs/data.nix
+++ home/pkgs/data.nix
@@ -4,9 +4,11 @@
     # csvkit # command-line CSV toolkit
     csvlens # command-line CSV viewer and transformer
     duckdb # SQL OLAP db management system
+    etcd # distributed key-value store
     python3Packages.faker # fake data generation library
     fx # JSON viewer
     gron # make JSON greppable
+    grpc-tools # grpc protocol buffer compiler tools
     # pgadmin4 # PostgreSQL administration and management GUI
     postgresql # PostgreSQL database server
     # python3Packages.pgcli # PostgreSQL CLI with autocompletion
