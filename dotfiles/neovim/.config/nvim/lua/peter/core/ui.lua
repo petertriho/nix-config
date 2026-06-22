59ea78733d77fe61dcaaf149224913c642338b2d
Peter Tri Ho <mail@petertriho.com>
2026-06-22 22:27:44 +1000

    refactor(nvim): replace noice with ui2

--- dotfiles/neovim/.config/nvim/lua/peter/core/ui.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/core/ui.lua
@@ -1,16 +1,68 @@
--- vim.o.cmdheight = 0
+vim.o.cmdheight = 0
 
--- require("vim._core.ui2").enable({
---     msg = {
---         target = "msg",
---         targets = {},
---         cmd = {
---             height = 0.5,
---         },
---         dialog = {
---             height = 0.5,
---         },
---         msg = { height = 0.3, timeout = 4000 },
---         pager = { height = 0.8 },
---     },
--- })
+local ui2 = require("vim._core.ui2")
+
+ui2.enable({
+    enable = true,
+    msg = {
+        targets = "msg",
+        cmd = {
+            height = 0.5,
+        },
+        dialog = {
+            height = 0.5,
+        },
+        msg = { height = 0.3, timeout = 4000 },
+        pager = { height = 0.8 },
+    },
+})
+
+local messages = require("vim._core.ui2.messages")
+
+if not messages.notify_msg_show_override then
+    messages.notify_msg_show_override = messages.msg_show
+
+    local notify_kinds = {
+        echo = true,
+        echoerr = true,
+        echomsg = true,
+        emsg = true,
+        lua_error = true,
+        lua_print = true,
+        quickfix = true,
+        rpc_error = true,
+        shell_ret = true,
+        undo = true,
+        wmsg = true,
+    }
+
+    local error_kinds = {
+        echoerr = true,
+        emsg = true,
+        lua_error = true,
+        rpc_error = true,
+    }
+
+    messages.msg_show = function(kind, content, replace_last, history, append, id, trigger)
+        if notify_kinds[kind] then
+            local text = table.concat(
+                vim.tbl_map(function(chunk)
+                    return chunk[2]
+                end, content),
+                ""
+            )
+
+            if text ~= "" then
+                vim.notify(text, error_kinds[kind] and vim.log.levels.ERROR or vim.log.levels.INFO, {
+                    id = id ~= 0 and ("ui2:" .. tostring(id)) or nil,
+                    replace = replace_last and id ~= 0 and ("ui2:" .. tostring(id)) or nil,
+                    title = kind == "lua_print" and "Lua" or "Message"
+                })
+            end
+
+            return
+        end
+
+        return messages.notify_msg_show_override(kind, content, replace_last, history, append, id, trigger)
+    end
+end
--- dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ui/noice.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/config/ui/noice.lua
@@ -1,5 +1,6 @@
 return {
     "folke/noice.nvim",
+    cond = false,
     event = "VeryLazy",
     keys = {
         {
