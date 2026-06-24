a553a0f98721e93f02dd4289b21dae350a446e74
Peter Tri Ho <mail@petertriho.com>
2026-06-24 15:26:49 +1000

    feat(nvim): add profiling support with snacks.nvim

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -1,1 +1,13 @@
+if vim.env.PROF then
+  local snacks = vim.fn.stdpath("data") .. "/site/pack/core/opt/snacks.nvim"
+  vim.opt.rtp:append(snacks)
+  require("snacks.profiler").startup({
+    startup = {
+      event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  })
+end
+
 require("peter")
