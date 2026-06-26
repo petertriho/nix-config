cd08d40307393d9ed5dc3bedf0fa25845a1a7943
Peter Tri Ho <mail@petertriho.com>
2026-06-27 00:04:02 +1000

    perf(nvim): hoist devicons import

--- dotfiles/neovim/.config/nvim/plugins/heirline-components/lua/heirline-components/components/fileicon.lua
+++ dotfiles/neovim/.config/nvim/plugins/heirline-components/lua/heirline-components/components/fileicon.lua
@@ -1,9 +1,11 @@
+local devicons = require("nvim-web-devicons")
+
 return {
     init = function(self)
         local filename = self.filename
         local extension = vim.fn.fnamemodify(filename, ":e")
         self.icon, self.icon_color =
-            require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+            devicons.get_icon_color(filename, extension, { default = true })
     end,
     provider = function(self)
         return self.icon and (self.icon .. " ")
