bbec45822a3860271bcc3ed114b68673fe0f5a0b
Peter Tri Ho <mail@petertriho.com>
2026-06-27 00:15:45 +1000

    perf(nvim): cache lsp client count

--- dotfiles/neovim/.config/nvim/plugins/heirline-components/lua/heirline-components/components/lspclientscount.lua
+++ dotfiles/neovim/.config/nvim/plugins/heirline-components/lua/heirline-components/components/lspclientscount.lua
@@ -1,11 +1,24 @@
 local conditions = require("heirline.conditions")
 
+local cache = {}
+
+vim.api.nvim_create_autocmd({ "LspAttach", "LspDetach" }, {
+    group = vim.api.nvim_create_augroup("heirline_lsp_count", { clear = true }),
+    callback = function(args)
+        cache[args.buf] = #vim.lsp.get_clients({ bufnr = args.buf })
+    end,
+})
+
 return {
     condition = conditions.lsp_attached,
     provider = function()
         local bufnr = vim.api.nvim_get_current_buf()
-        local client_count = #vim.lsp.get_clients({ bufnr = bufnr })
-        return "󰒓 " .. client_count
+        local count = cache[bufnr]
+        if count == nil then
+            count = #vim.lsp.get_clients({ bufnr = bufnr })
+            cache[bufnr] = count
+        end
+        return "󰒓 " .. count
     end,
     update = { "LspAttach", "LspDetach" },
 }
