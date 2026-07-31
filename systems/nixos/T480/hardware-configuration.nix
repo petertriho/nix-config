284ca3b52309f2205cfefa34b2295918eb9abd03
Peter Tri Ho <mail@petertriho.com>
2026-08-01 00:45:23 +1000

    perf(nix): increase t480 perf on battery

--- systems/nixos/T480/hardware-configuration.nix
+++ systems/nixos/T480/hardware-configuration.nix
@@ -66,7 +66,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 85;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 45;
+        CPU_MAX_PERF_ON_BAT = 70;
 
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
