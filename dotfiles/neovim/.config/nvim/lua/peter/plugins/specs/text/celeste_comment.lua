71f65856c8a4fb5374e26042b237dea04342d96d
Peter Tri Ho <mail@petertriho.com>
2026-08-21 21:55:15 +1000

    feat(nvim): add `celeste_comment.nvim` plugin

---
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/specs/text/celeste_comment.lua
@@ -0,0 +1,20 @@
+return {
+    "celeste3z/celeste_comment.nvim",
+    keys = {
+        { "gc", mode = { "n", "x", "o" } },
+        { "gb", mode = { "n", "x", "o" } },
+        "gcc",
+        "gbc",
+        "gco",
+        "gcO",
+        "gcA",
+    },
+    opts = {
+        keep_selection = "accurate",
+        mappings = {
+            line_add_below = "gco",
+            line_add_above = "gcO",
+            line_add_eol = "gcA",
+        },
+    },
+}
--- dotfiles/neovim/.config/nvim/lua/peter/plugins/specs/treesitter/nvim-ts-context-commentstring.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/plugins/specs/treesitter/nvim-ts-context-commentstring.lua
@@ -1,5 +1,6 @@
 return {
     "JoosepAlviste/nvim-ts-context-commentstring",
+    enabled = false,
     event = "User LoadedNvimTreesitter",
     config = function()
         vim.g.skip_ts_context_commentstring_module = true
